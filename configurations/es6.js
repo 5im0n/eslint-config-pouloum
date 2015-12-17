2eb0459a6887a671f6562bd7800a98c2bab7ffa6
Simon MAHÉ <mahe.simon@gmail.com>
2015-12-17 20:12:34 +0100

    refactor(es6): move ecmaFeatures object to configurations/es6.js

--- configurations/es6.js
+++ configurations/es6.js
@@ -2,8 +2,57 @@
 
 module.exports = {
   'extends': [
+    'pouloum/configurations/es5',
     'pouloum/rules/ecmascript-6'
   ],
+  ecmaFeatures: {
+    // Enable arrow functions
+    arrowFunctions: true,
+    // Enable binary literals
+    binaryLiterals: true,
+    // Enable let and const (aka block bindings)
+    blockBindings: true,
+    // Enable classes
+    classes: true,
+    // Enable default function parameters
+    defaultParams: true,
+    // Enable destructuring
+    destructuring: true,
+    // Enable for-of loops
+    forOf: true,
+    // Enable generators
+    generators: false,
+    // Enable modules and global strict mode
+    modules: false,
+    // Enable computed object literal property names
+    objectLiteralComputedProperties: true,
+    // Enable duplicate object literal properties in strict mode
+    objectLiteralDuplicateProperties: false,
+    // Enable object literal shorthand methods
+    objectLiteralShorthandMethods: true,
+    // Enable object literal shorthand properties
+    objectLiteralShorthandProperties: true,
+    // Enable octal literals
+    octalLiterals: true,
+    // Enable the regular expression u flag
+    regexUFlag: true,
+    // Enable the regular expression y flag
+    regexYFlag: true,
+    // Enable the rest parameters
+    restParams: true,
+    // Enable the spread operator
+    spread: true,
+    // Enable super references inside of functions
+    superInFunctions: true,
+    // Enable template strings
+    templateStrings: true,
+    // Enable code point escapes
+    unicodeCodePointEscapes: true,
+    // allow return statements in the global scope
+    globalReturn: false,
+    // Enable JSX
+    jsx: false
+  },
   rules: {
      // Disallow dangling underscores in identifiers
     'no-underscore-dangle': 2
--- rules/ecmascript-6.js
+++ rules/ecmascript-6.js
@@ -8,57 +8,6 @@
 'use strict';
 
 module.exports = {
-  env: {
-    es6: false
-  },
-  ecmaFeatures: {
-    // Enable arrow functions
-    arrowFunctions: true,
-    // Enable binary literals
-    binaryLiterals: true,
-    // Enable let and const (aka block bindings)
-    blockBindings: true,
-    // Enable classes
-    classes: true,
-    // Enable default function parameters
-    defaultParams: true,
-    // Enable destructuring
-    destructuring: true,
-    // Enable for-of loops
-    forOf: true,
-    // Enable generators
-    generators: false,
-    // Enable modules and global strict mode
-    modules: false,
-    // Enable computed object literal property names
-    objectLiteralComputedProperties: true,
-    // Enable duplicate object literal properties in strict mode
-    objectLiteralDuplicateProperties: false,
-    // Enable object literal shorthand methods
-    objectLiteralShorthandMethods: true,
-    // Enable object literal shorthand properties
-    objectLiteralShorthandProperties: true,
-    // Enable octal literals
-    octalLiterals: true,
-    // Enable the regular expression u flag
-    regexUFlag: true,
-    // Enable the regular expression y flag
-    regexYFlag: true,
-    // Enable the rest parameters
-    restParams: true,
-    // Enable the spread operator
-    spread: true,
-    // Enable super references inside of functions
-    superInFunctions: true,
-    // Enable template strings
-    templateStrings: true,
-    // Enable code point escapes
-    unicodeCodePointEscapes: true,
-    // allow return statements in the global scope
-    globalReturn: false,
-    // Enable JSX
-    jsx: false
-  },
   rules: {
     // require braces in arrow function body
     'arrow-body-style': [2, 'always'],
