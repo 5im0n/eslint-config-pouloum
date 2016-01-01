f0e74e6a8dbc1f32a3faca197b797e0968e0acb9
Simon MAHÉ <mahe.simon@gmail.com>
2016-01-01 14:31:08 +0100

    test(angularRules): ensure all angular rules begin
    with `angular/`

--- test/angular-rules.spec.js
+++ test/angular-rules.spec.js
@@ -9,6 +9,7 @@ import rulesConventions from '../rules/angular/conventions';
 import rulesWrappers from '../rules/angular/wrappers';
 
 
+
 /* Angular Rules */
 
 test('Possible errors', t => {
@@ -16,27 +17,65 @@ test('Possible errors', t => {
   t.true(isPlainObj(rulesPossibleErrors.rules));
 });
 
+Object.keys(rulesPossibleErrors.rules).forEach(a => {
+  checkRuleName(a);
+});
+
+
 test('Best Practices', t => {
   t.true(isPlainObj(rulesBestPractices));
   t.true(isPlainObj(rulesBestPractices.rules));
 });
+
+Object.keys(rulesBestPractices.rules).forEach(a => {
+  checkRuleName(a);
+});
+
 
 test('Deprecated Features', t => {
   t.true(isPlainObj(rulesDeprecatedFeatures));
   t.true(isPlainObj(rulesDeprecatedFeatures.rules));
 });
 
+Object.keys(rulesDeprecatedFeatures.rules).forEach(a => {
+  checkRuleName(a);
+});
+
+
 test('Naming', t => {
   t.true(isPlainObj(rulesNaming));
   t.true(isPlainObj(rulesNaming.rules));
 });
+
+Object.keys(rulesNaming.rules).forEach(a => {
+  checkRuleName(a);
+});
+
 
 test('Conventions', t => {
   t.true(isPlainObj(rulesConventions));
   t.true(isPlainObj(rulesConventions.rules));
 });
 
+Object.keys(rulesConventions.rules).forEach(a => {
+  checkRuleName(a);
+});
+
+
 test('Wrappers', t => {
   t.true(isPlainObj(rulesWrappers));
   t.true(isPlainObj(rulesWrappers.rules));
 });
+
+Object.keys(rulesWrappers.rules).forEach(a => {
+  checkRuleName(a);
+});
+
+
+/* -------------------- */
+
+function checkRuleName(value) {
+  test('Rule name', t => {
+    t.true(value.indexOf('angular/') >= 0);
+  });
+}
